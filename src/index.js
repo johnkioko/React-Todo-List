cacd5f74527309a4ce82e3342a7fb888b8276734
john <kiokoj81@gmail.com>
2022-11-10 10:50:30 +0300

    Refactor code to use React Hooks

--- src/index.js
+++ src/index.js
@@ -1,10 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
-
-import TodoContainer from "./components/TodoContainer";
+import TodoContainer from "./functionBased/components/TodoContainer";
 
 //stylesheet
-import "./App.css";
+import "./functionBased/App.css";
 
 ReactDOM.render(
   <React.StrictMode>
